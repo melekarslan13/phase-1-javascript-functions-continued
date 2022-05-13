544a247b0e1105a580304878c3dede7a950bbbca
melekarslan13 <melekarslan.ie@gmail.com>
2022-05-14 00:43:06 +0300

    functions continued finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(str){
+if(str !== undefined)
+    return `This Saturday, I want to ${str}!`
+else
+    return "This Saturday, I want to roller-skate!"
+}
+function mondayWork(str){
+    if(str !== undefined)
+        return `This Monday, I will ${str}.`
+    else
+        return "This Monday, I will go to the office."
+    }
+function   wrapAdjective(a){
+
+if( a==="*")
+    return function(str){
+        return `You are *${str}*!`
+    }
+if (a==="||")
+    return function(str){
+        return `You are ||${str}||!`
+    }
+}
